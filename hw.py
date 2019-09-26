65ee7279b68559e4c296e3b242f17f273ffd1f79
Ryan Johnson <ryan.johnson.1315@gmail.com>
2019-09-25 23:34:34 -0600

    Uncommented announcment stuff. All working

--- hw.py
+++ hw.py
@@ -1,4 +1,5 @@
 from canvasapi import Canvas
+import canvasapi
 from utils import *
 
 def run():
@@ -10,18 +11,35 @@ def run():
     # Canvas object and students classes
     canvas = Canvas(config['url'], config['api'])
     courses = config['classes']
-
-
-    # if the user sets the weeks or days -> use it, if not -> use default of 21 days
     days = args.days if args.weeks == 0 else args.weeks * 7
 
     if args.reset:
         print('Resetting output space')
         config['longest_desc'] = 0
         config['longest_clas'] = 0
-
+    
     for course_number in courses:
         course = canvas.get_course(course_number)
+        
+
+        #announcements = course.get_announcements()
+        #announcements = canvas.get_announcements(context_codes=[str(course_number)])
+        #print(announcements._elements)
+
+        #
+        #for i in announcements:
+        #    import json
+        #    #print(json.dumps(i.attributes))
+        #    date = get_date(i.created_at)
+        #    delta = now - date
+        #    print(f'Days: {delta.days} | {i.html_url}')
+     
+     
+     
+
+
+
+        '''This is working from here down'''
         assignments = course.get_assignments()
         for a in assignments:
             try:
